6ab8338c15832b3b995d8dcc93261a779203d203
Steve Newsted <newsted@gmail.com>
2025-04-29 00:12:00 -0400

    more goofing around

--- list_vms.sh
+++ list_vms.sh
@@ -1,4 +1,7 @@
-{
-  echo '{"jsonrpc": "2.0", "method": "initialize", "params": {"protocolVersion": "1.0", "capabilities": {}, "clientInfo": {"name": "test-client", "version": "1.0"}}, "id": 1}'
-  echo '{"jsonrpc": "2.0", "method": "tools/call", "params": {"name": "list_vms", "arguments": {}}, "id": 2}'
-} | python3 kvm_mcp_server.py 
+#!/bin/bash
+
+# Send initialization request
+echo '{"jsonrpc": "2.0", "method": "initialize", "params": {"protocolVersion": "1.0", "capabilities": {}, "clientInfo": {"name": "test-client"}}, "id": 1}' | python3 kvm_mcp_server.py
+
+# List all VMs
+echo '{"jsonrpc": "2.0", "method": "tools/call", "params": {"name": "list_vms", "arguments": {}}, "id": 2}' | python3 kvm_mcp_server.py 
