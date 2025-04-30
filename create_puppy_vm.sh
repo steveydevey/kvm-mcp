b07352bb920a7872e476c306aca250f5e9a159b7
Steve Newsted <newsted@gmail.com>
2025-04-29 22:53:43 -0400

    cleanup of old stuff

--- create_puppy_vm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Send initialization request
-echo '{"jsonrpc": "2.0", "method": "initialize", "params": {"protocolVersion": "1.0", "capabilities": {}, "clientInfo": {"name": "test-client"}}, "id": 1}' | python3 kvm_mcp_server.py
- 
-# Send VM creation request with minimal parameters, using defaults from config
-echo '{"jsonrpc": "2.0", "method": "tools/call", "params": {"name": "create_vm", "arguments": {}}, "id": 2}' | python3 kvm_mcp_server.py 
